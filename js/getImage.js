f9d5737e607645b04aca496edaefc7e24ab0eed6
Reuben Reyes <reuben.a.reyes@gmail.com>
2017-08-14 11:47:56 -0700

    first version ready

--- js/getImage.js
+++ js/getImage.js
@@ -1,36 +1,25 @@
-window.onload = () => {
-	// load an image on landing
-	$( '#loading' ).fadeOut( 300, () => {
-		getImage();
-	} );
+function transition() {
+	/* clear any messages being displayed */
+	$( '#message' ).html( '' );
+	let t = [
+		'Retrieving borks...',
+		'Consulting dog therapy clinic...',
+		'Boofs are imminent.',
+		'They\'re good dogs, Brent.',
+	]
 
-	// add click listener to "new image" button
-	document.querySelector( '#refreshImg' ).addEventListener( 'click', () => {
-
-		// clear error message if any
-		$( '.message' ).html( '' );
-
-		// fade out video and buttons
+	let render = randRange( 0, t.length, false );
 		$( '#controls' ).fadeOut( 150, () => {
 			$( '#target' ).fadeOut( 150, () => {
-				// get loading message
-				let t = [
-					'Retrieving borks...',
-					'Consulting dog therapy clinic...',
-					'Boofs are imminent.',
-					'They\'re good dogs, Brent.',
-				]
-				let render = randRange( 0, t.length );
-				$( '#loading h2' ).html( t[render] );
-
-				// fade in loading message, then make API request
-				$( '#loading' ).fadeIn( 150, () => {
-					getImage();
+				$( '#title' ).slideDown( () => {
+					$	( '#loadingMessage' ).hide().html( t[render] ).fadeIn( 150, () => {
+						getImage();
+					} );
 				} );
 			} );
 		} );
-	} );
-};
+
+}
 
 function randRange( min, max, incl = true ) {
   /* Returns a random integer between min and max.
@@ -40,18 +29,14 @@ function randRange( min, max, incl = true ) {
   return Math.floor( Math.random ( max - min + 1 ) * max );
 }
 
-function dequote( link ) {
-	return JSON.stringify( link ).replace( /["']/gi, '' );
-}
-
 function getSearchParams( tags ) {
 	/* generates random search parameters always including an element in tags
 	 * one or many elements in mods may be appended to the search
 	 */
-	let tag = randRange( 0, tags.length );
-  let mods = [ 'cute', 'funny', ]
+	var tag = randRange( 0, tags.length );
+  var mods = [ 'cute', 'funny', 'corgis', 'pugs', 'doggos', 'puppers', ]
 
-  let params = [ tags[tag], ];
+  var params = [ tags[tag], ];
   mods.forEach( e => {
     if( randRange( 0, 1 ) ) {
       params.push( '+'.concat( e ) );
@@ -61,10 +46,45 @@ function getSearchParams( tags ) {
   return params.join( '' ) ;
 }
 
+function curate( gif, tags, i ) {
+	/* searches the tags and title of a random image contained in JSON response
+	 * and returns an object indicating pass/fail, and  index of the image tested
+	 * an image passes if either tags or title conains an element in .tags
+	 *
+	 * ISSUES:
+	 * RegExp.ignoreCase doesn't seem to work properly.
+	 * haven't figured out why; can bypass by forcing lowercase on String.match()
+	 */
+	var pass = false;
+
+	/* search tags first */
+	if( gif[i].tags ) {
+		pass = tags.some( ele => {
+			let re = new RegExp( ele, 'im' );
+			return gif[i].tags.join().toLowerCase().match( ele );
+		} );
+	}
+
+	/* only search title if nothing was found in tags */
+	if( gif[i].title && !pass ) {
+		pass = tags.some( ele => {
+			let re = new RegExp( ele, 'im' );
+			return gif[i].title.toLowerCase().match( ele );
+		} );
+	}
+
+	return { 'pass': pass, 'index': i };
+}
+
 function getImage() {
-	let tags = [ 'dog', 'dogs', 'puppy', 'puppies' ];
-  let mods = getSearchParams( tags );
-	let url = 'https://api.gfycat.com/v1test/gfycats/search?search_text=' + mods + '&count=100';
+	/* sends a request to Gfycat API endpoint
+	 * curates response for images with tag/title containing "dog" or something
+	 * image is always randomly selected
+	 * displays image and handles page transitions once loaded
+	 */
+	var tags = [ 'dog', 'dogs', 'puppy', 'puppies' ]; // initial search tags
+  var mods = getSearchParams( tags ); // search modifiers
+	var url = 'https://api.gfycat.com/v1test/gfycats/search?search_text=' + mods + '&count=100';
 
 	$.ajax( {
 	  dataType: "json",
@@ -72,70 +92,73 @@ function getImage() {
 		timeout: 2000
 	} )
 		.done( data => {
-			// render random image
-			let i = randRange( 0, data.gfycats.length, false );
-			let pass = false;
-			let gif = data.gfycats[i];
-			console.log( 'TITLE: ' + gif.title );
-			console.log( 'TAGS: ' + gif.tags );
+			var c = { 'pass': false, 'index': null }; // to be mutated by curate()
+			var blacklist = []; // images that don't pass curate() pushed to blacklist
+			var i; // index of image being passed to curate()
 
-			if( gif.tags ) {
-				console.log( 'processing tags' );
-				pass = tags.some( ele => {
-					let re = new RegExp( ele, 'gi' );
-					console.log( gif.tags.join( ' ' ).match( ele ) );
-					return gif.tags.join( ' ' ).match( ele );
+			try {
+				while( !c.pass ) {
+					let calls = 0;
+					while( true ) {
+						/* stop execution if loop runs through all images in search */
+						calls++;
+						if( calls > 100 ) { throw 'NO MATCHES'; }
+
+						/* get a random image from search, exit if it's unique */
+						i = randRange( 0, data.gfycats.length, false );
+						if( !blacklist.includes( i ) ) { break; }
+					}
+
+					c = curate( data.gfycats, tags, i );
+					if( !c.pass ) { blacklist.push( c.index ); }
+
+				}
+			}
+			catch( e ) {
+				/* client-side timeout error */
+				$( '#controls' ).fadeIn( 150 );
+				$( '#message' ).html( 'Couldn\'t find any puppers in this search! Try again? (ERR: ' + e + ')'  );
+			}
+
+			var gif = data.gfycats[c.index]; // image to be rendered
+
+			/* If image is greater than 5:4 aspect ratio
+			 * make media height-responsive. Otherwise, width-responsive.
+			 */
+			var a = ( gif.height >= .8*gif.width ) ? 'max-height' : 'width';
+			var b = ( gif.height < .8*gif.width ) ? 'max-width' : 'height';
+			$( '#target ').css( a, 'inherit' );
+			$( '#target' ).css( b, 'auto' );
+
+			/* render image and control panel */
+			$( '#target' ).attr( 'src', gif.mp4Url );
+			$( '#controls' ).css( 'width', $( '#target' ).css( 'width' ) + 'px' );
+
+			/* render image additional info */
+			$( '#mp4Text' ).attr( 'value', gif.mp4Url );
+			$( '#mobileText' ).attr( 'value', gif.mobileUrl );
+			$( '#gifText' ).attr( 'value', gif.gifUrl );
+			$( '#webmText' ).attr( 'value', gif.webmUrl );
+
+			/* update twitter link */
+			$( '#tweet' ).attr(
+				 'href',
+				 'https://twitter.com/intent/tweet?hashtags=InternetTherapyDog&related=freecodecamp&text=' + gif.mobileUrl
+			 );
+
+			 /* page transitions */
+			$( '#loadingMessage' ).fadeOut( 150, () => {
+				$( '#title' ).slideUp( () => {
+					$( '#target' ).show( () => {
+						$( '#controls' ).slideDown();
+					} );
 				} );
-				console.log( 'IN TAGS... pass = ' + pass );
-			}
-
-			if( gif.title && !pass ) {
-				console.log( 'processing title' );
-				pass = tags.some( ele => {
-					let re = new RegExp( ele, 'gi' );
-					console.log( gif.title.match( ele ) );
-					return gif.title.match( ele );
-				} );
-				console.log( 'IN TITLE... pass = ' + pass );
-			}
-
-			if( pass ) {
-				// render image and show link
-				// $( '#target' ).html( '<img src=' + JSON.stringify( gif.max5mbGif )	+ '>'	);
-				$( '#target' ).attr( 'src', gif.mp4Url );
-				$( '#target' ).attr( 'width', gif.width );
-				$( '#target' ).attr( 'height', gif.height );
-				$( '#controls' ).css( 'width', gif.width + 'px' );
-
-				// DEBUG
-				$( '#debug p' ).html( gif.width + 'x' + gif.height );
-				$( '#mp4Text' ).attr( 'value', gif.mp4Url );
-
-				// update twitter link
-				$( '#tweet' ).attr(
-					 'href',
-					 'https://twitter.com/intent/tweet?hashtags=InternetTherapyDog&related=freecodecamp&text=' + gif.mobileUrl
-				 );
-
-				 $( 'html, body' ).animate( {
-				 		scrollTop: $( '#media' ).offset().top
-				 }, 500, 'easeInCubic' );
-
-				 // fade in after everything is done
-				 $( '#controls' ).fadeIn( 150 );
-				 $( '#target' ).fadeIn( 150 );
-				 $( '#loading' ).fadeOut( 150 );
-				 console.log( 'rendering finished' );
-			}
-
-			else {
-				console.log( 'Failed curator test. Recursing...' );
-				getImage();
-			}
+			} );
 		} )
 
     .fail( data => {
+			/* server-side error */
 			$( '#controls' ).fadeIn( 150 );
-			$( '.message' ).html( 'Whoops! There was a problem loading this image.' + '(' + ')' );
+			$( '#message' ).html( 'Whoops! Looks like the doggo server had a hiccup.' );
 		} );
 }
